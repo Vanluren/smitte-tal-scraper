00061819104685293e955ecdcc29d17ac72eb8e2
Villads Nielsen <villadsvalur@gmail.com>
2020-03-11 17:57:50 +0100

    added a deploy field to the package, check for port in .env before launching

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { fetchAndSaveNumbers } from "./scraper";
 import db from "./database";
 import "./cron";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res, next) => {
