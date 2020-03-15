7d3aa71b0adcfd367096a43a1ada476dcadb4434
Villads Nielsen <villadsvalur@gmail.com>
2020-03-15 16:00:36 +0100

    update cron job

--- src/cron.js
+++ src/cron.js
@@ -1,8 +1,8 @@
-// import cron from "node-cron";
-// import moment from "moment";
-// import { fetchAndSaveNumbers } from "./scraper/scraper";
+import cron from "node-cron";
+import moment from "moment";
+import { fetchAndSaveNumbers } from "./scraper/scraper";
 
-// cron.schedule("0 */1 * * *", () => {
-//   console.log(`Running cron job - scraping stps.dk ${moment().format()}`);
-//   return fetchAndSaveNumbers();
-// });
+cron.schedule("0 */2 * * *", () => {
+  console.log(`Running cron job - scraping worldometer ${moment().format()}`);
+  return fetchAndSaveNumbers();
+});
